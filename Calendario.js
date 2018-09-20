b3a2709aca1ab02b4f753f311c68ff45b0c50df8
alebrenes8 <43450221+alebrenes8@users.noreply.github.com>
2018-09-20 17:08:08 -0600

    Add files via upload

---
+++ Calendario.js
@@ -0,0 +1,4 @@
+var monthNames = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+var dayNames = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+var calendar = new CalendarPopup("popupCal");
+calendar.showNavigationDropdowns();
